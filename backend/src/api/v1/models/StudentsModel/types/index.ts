0f1fe39d5b739bdaafce5bd4b62be23fd021351e
satilpereira <satil.prog@gmail.com>
2025-02-04 00:53:10 -0300

    feat: Add createStudent method to StudentsModel with error handling and logging

--- backend/src/api/v1/models/StudentsModel/types/index.ts
+++ backend/src/api/v1/models/StudentsModel/types/index.ts
@@ -6,5 +6,33 @@ declare module '@models/StudentsModel' {
      * @throws Error if the ping fails.
      */
     ping(): Promise<any>;
+
+    /**
+     * @description Create a new student.
+     * @returns The created student.
+     * @throws Error if the student creation fails.
+     */
+    createStudent({
+      name,
+      email,
+      ra,
+      cpf,
+    }: {
+      name: string;
+      email: string;
+      ra: string;
+      cpf: string;
+    }): Promise<
+      | {
+          insertedId: number;
+          acknowledged: boolean;
+        }
+      | {
+          insertedId: -1;
+          acknowledged: false;
+          error: 'ra_already_exists';
+        }
+      | Error
+    >;
   }
 }
